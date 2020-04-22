562bee86967041e32872f3bb9cff29bf0343b7fb
MaxAntony <maxcamito@gmail.com>
2020-04-22 11:55:21 -0500

    conectado con data

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -1,7 +1,7 @@
+const data = require('../config/fakeDB.config');
 const dataCtrl = {};
-
 dataCtrl.getDefault = (req, res) => {
-  res.render('hello');
+  res.render('hello', { data });
 };
 
 module.exports = dataCtrl;
